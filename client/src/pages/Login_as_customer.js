8ce05889d8cbe8d16d237869db188e1edef5c5fb
cristina <01fe22bca111@kletech.ac.in>
2024-02-18 23:51:55 +0530

    login

--- client/src/pages/Login_as_customer.js
+++ client/src/pages/Login_as_customer.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
-
+import user_icon from "../Images/Assets/person.png";
+import email_icon from "../Images/Assets/email.png";
+import password_icon from "../Images/Assets/password.png";
+import "../pages/signup.css";
 const Login_as_customer = () => {
+  const [action, setAction] = useState("Sign Up");
   return (
     <Layout>
-      <h1>This is Customer Login</h1>
+      <div className="container">
+        <div className="header">
+          <div className="text">{action}</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          {action === "Login" ? (
+            <div></div>
+          ) : (
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Username" />
+            </div>
+          )}
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id" />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        {action === "Sign Up" ? (
+          <div></div>
+        ) : (
+          <div className="forgot-password">
+            Lost Password? <span>Click Here</span>
+          </div>
+        )}
+
+        <div className="submit-container">
+          <div
+            className={action === "Login" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Sign Up");
+            }}
+          >
+            Sign Up
+          </div>
+          <div
+            className={action === "Sign Up" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Login");
+            }}
+          >
+            Login
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
