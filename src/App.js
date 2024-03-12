e2967c16e6e01761d202e5e2ad2648f5c12a3f26
nikita <01fe22bca064@kletech.ac.in>
2024-03-12 03:24:51 -0700

    this is my first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Warmup from "./pages/Warmup";
+import Day1 from "./pages/Day1";
+import Day2 from "./pages/Day2";
+import Restday from "./pages/Restday";
+import Schedule from "./pages/Schedule";
+import Payment from "./pages/Payment";
+import Logout  from  "./pages/Logout";
+import Pagenotfound from "./pages/Pagenotfound";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Warmup" element={<Warmup />} />
+          <Route path="/Day1" element={<Day1 />} />
+          <Route path="/Day2" element={<Day2 />} />
+          <Route path="/Restday" element={<Restday />} />
+          <Route path="/Schedule" element={<Schedule />} />
+          <Route path="/Payment" element={<Payment />} />
+          <Route path="/Logout" element={<Logout/>} />
+          <Route path="/*" element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
